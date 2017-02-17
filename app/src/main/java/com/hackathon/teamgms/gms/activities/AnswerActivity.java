6ac201b45b8cd3e71cfabee7516a9a3d854ab939
daeun <kde419eee@naver.com>
2017-02-17 23:46:50 +0900

    chooseAnswer

---
+++ app/src/main/java/com/hackathon/teamgms/gms/activities/AnswerActivity.java
@@ -0,0 +1,15 @@
+package com.hackathon.teamgms.gms.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hackathon.teamgms.gms.R;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+    }
+}
