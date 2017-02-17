2118b8e2303c052050358a7872ff16054a65589d
HYUNJEONGH <hwahgyj@outlook.com>
2017-02-17 18:43:57 +0900

    initial commit
    프로젝트 생성

---
+++ app/src/main/java/com/hackathon/teamgms/gms/activities/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hackathon.teamgms.gms.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hackathon.teamgms.gms.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //addsingle ,addvalue 구분
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //listener
+//        for ()
+        //TODO: update user interface
+    }
+}
